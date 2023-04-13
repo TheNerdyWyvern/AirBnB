111e0cbacda2efb341491f075f486a34cb7fd4ee
TheNerdyWyvern <raynorhoobler@gmail.com>
2023-04-13 16:02:23 -0700

    Added common error handling

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,7 @@ const cookieParser = require('cookie-parser');
 const {environment} = require('./config');
 const isProduction = environment === 'production';
 const routes = require('./routes');
+const { ValidationError } = require('sequelize');
 
 const app = express();
 
@@ -15,6 +16,40 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(express.json());
 app.use(routes);
+
+//catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+    const err = new Error("Thre requseted resource couldn't be found.");
+    err.title = "Resource Not Found";
+    err.errors = { message: "The requested resource couldn't be found."};
+    err.status = 404;
+    next(err);
+});
+
+//process sequelize errors
+app.use((err, _req, _res, next) => {
+    if (err instanceof ValidationError) {
+        let errors = [];
+        for (let error of err.errors) {
+            errors[error.path] = error.message;
+        }
+        err.title = 'Validation Error';
+        err.errors = errors;
+    }
+    next(err);
+});
+
+//error formatter
+app.use((err, _req, res, _next) => {
+    res.status(err.status || 500);
+    console.error(err);
+    res.json({
+        title: err.title || 'Server Error',
+        message: err.message,
+        errors: err.errors,
+        stack: isProduction ? null : err.stack
+    });
+});
 
 //security middleware
 if (!isProduction) {
