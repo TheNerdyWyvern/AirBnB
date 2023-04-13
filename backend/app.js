44d3d90e61ae3d1ff9c7e3b5a6c77a296438663a
TheNerdyWyvern <raynorhoobler@gmail.com>
2023-04-13 14:22:52 -0700

    Initialize Express and Sequelize with CSRF Production

---
+++ backend/app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+require('express-async-errors');
+const morgan = require('morgan');
+const cors = require('cors');
+const csurf = require('csurf');
+const helmet = require('helmet');
+const cookieParser = require('cookie-parser');
+const {environment} = require('./config');
+const isProduction = environment === 'production';
+const routes = require('./routes');
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(cookieParser());
+app.use(express.json());
+app.use(routes);
+
+//security middleware
+if (!isProduction) {
+    //enable cors only in development
+    app.use(cors());
+}
+app.use(
+    helmet.crossOriginResourcePolicy({
+        policy: "cross-origin"
+    })
+);
+
+//set the _csurf token and create req.csurfToken method
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
+
+module.exports = app;
