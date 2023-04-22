548a1f73f872a9228a0b640cd720337ee508f5d3
TheNerdyWyvern <raynorhoobler@gmail.com>
2023-04-22 15:03:30 -0700

    delete spotImage and delete reviewImage finished

---
+++ backend/routes/api/spot-images.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const { check } = require('express-validator');
+
+const { requireAuth } = require('../../utils/auth');
+const { Review, ReviewImage, Spot, User } = require('../../db/models');
+const { handleValidationErrors } = require('../../utils/validation');
+
+const verifySpotImage = async (req, _res, next) => {
+    const spotImages = await SpotImage.findAll();
+
+    const spotImage = spotImages.find(spotImage => spotImage.id == req.params.id);
+
+    if (!spotImage) {
+        const err = new Error("Couldn't find a Spot Image with the specified id");
+        err.title = "Resource Not Found";
+        err.errors = { message: "Spot Image couldn't be found."};
+        err.status = 404;
+        return next(err);
+    }
+
+    next();
+}
+
+const router = express.Router();
+
+router.delete('/:id', requireAuth, verifySpotImage, async (req, res) => {
+    const spotImage = await SpotImage.findByPk(req.params.id);
+
+    if (spotImage.userId == req.user.id) {
+        await spotImage.destroy();
+
+        res.json({ message: 'Successfully deleted' });
+    }
+    else {
+        const err = Error("Forbidden");
+        err.errors = { message: "Forbidden"};
+        err.status = 403;
+        err.title = "Forbidden";
+        return next(err);
+    }
+});
+
+module.exports = router;
