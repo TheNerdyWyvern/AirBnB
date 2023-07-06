d8d512d58edff13da893e1d6bae0952ce15eb42c
TheNerdyWyvern <raynorhoobler@gmail.com>
2023-07-06 00:01:31 -0700

    attempted adding a home icon

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -10,7 +10,7 @@ function Navigation({ isLoaded }){
   return (
     <ul>
       <li>
-        <NavLink exact to="/">Home</NavLink>
+        <NavLink exact to="/"><img src="Spade.ico" alt="Home Icon" style={{"object-fit": "cover"}}/></NavLink>
       </li>
       {isLoaded && (
         <li>
